035bef7bfb4c9eff0a56f4aa89c779b43ae3f9b1
anyangml <anyangpeng.ca@gmail.com>
2023-06-29 22:49:46 -0400

    feat: add composite pattern

---
+++ Composite/README.md
@@ -0,0 +1,2 @@
+# Composite Pattern
+![Composite_UML](http://www.plantuml.com/plantuml/proxy?cache=no&src=https://raw.githubusercontent.com/anyangml/design_patterns/main/Composite/uml.txt)
---
+++ Composite/composite.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+class MenuComponent(ABC):
+
+    def add(self) -> None:
+        pass
+
+    def remove(self) -> None:
+        pass
+
+    def getChild(self) -> MenuComponent:
+        pass
+
+    def getPrice(self) -> float:
+        pass
+
+class Menu(MenuComponent):
+
+    def __init__(self) -> None:
+        self.menuComponents = []
+
+    def add(self, menuComponent: MenuComponent) -> None:
+        self.menuComponents.append(menuComponent)
+
+    def remove(self, menuComponent: MenuComponent) -> None:
+        self.menuComponents.remove(menuComponent)
+
+    def getChild(self, i: int) -> MenuComponent:
+        return self.menuComponents[i]
+
+class MenuItem(MenuComponent):
+
+    def __init__(self, price: float) -> None:
+        self.price = price
+
+    def getPrice(self) -> float:
+        return self.price
+
+if __name__ == "__main__":
+    lunc_menu = Menu()
+    tuna_wrap = MenuItem(5.0)
+    fries = MenuItem(3.0)
+    lunc_menu.add(tuna_wrap)
+    lunc_menu.add(fries)
+    print(lunc_menu.getChild(0).getPrice())
